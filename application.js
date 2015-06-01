f23c27ab2f8429aa767f72c34d777eb0008d8258
chiu <chiucode@gmail.com>
2015-06-01 19:33:04 +0000

    almost working

---
+++ application.js
@@ -0,0 +1,38 @@
+
+
+function myFunction() {
+
+  userMoney = 100;
+// var randomNumber = 1;
+var randomNumber = Math.floor((Math.random() * 3) + 1);
+
+var betAmount = $("#betAmount");
+if (betAmount != null) {
+
+  "You bet the following amount: " + betAmount + "! How are you today?";
+}
+
+userMoney = userMoney - betAmount;
+
+
+var userGuess = $("userGuess");
+if (userGuess != null) {
+
+  "You guessed the following number: " + userGuess + "! How are you today?";
+}
+
+if (userGuess == randomNumber){
+  alert("You won the bet.");
+  userMoney = userMoney + betAmount * 2;
+} else if(userGuess == randomNumber + 1 || userGuess == randomNumber -1){
+  alert("You were off by 1: keep your bet");
+  userMoney = userMoney + betAmount;
+}  else {
+  alert("You lost.");
+  userMoney = userMoney;
+}
+
+alert("Your remaining money is:" + userMoney);
+$("#results").text(userMoney.val());
+
+}
